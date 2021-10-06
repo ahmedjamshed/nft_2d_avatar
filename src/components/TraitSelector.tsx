03f75944a132a5c4e2c6be36ed6ef9feed3dbf10
Ahmed <ahmedjamshed3@gmail.com>
2021-10-06 13:32:36 -0400

    trait selector

--- src/components/TraitSelector.tsx
+++ src/components/TraitSelector.tsx
@@ -1,25 +1,30 @@
+import classNames from 'classnames';
 import { Trait } from '../types';
 import classes from './TraitSelector.module.scss'
 
 interface TraitSelectorProps {
-    trait: Trait
+  trait: Trait
 }
 
 const TraitSelector = (props: TraitSelectorProps) => {
-    const { trait } = props
-    const urlPrefix = process.env.PUBLIC_URL + "/assets/traits";
-    return (
-      <div
-        className={classes.TraitSelector}
-        data-trait={trait.imageName}
-      >
-        <img
-          src={urlPrefix + "/" + trait.imageName + ".png"}
-          alt={trait.imageName}
-        />
-        
-      </div>
-    );
+  const { trait } = props
+  const urlPrefix = process.env.PUBLIC_URL + "/assets/traits";
+  return (
+    <div
+      id="TraitSelector"
+      className={classNames(classes.TraitSelector, {
+        [classes.selected]: true
+      })}
+      onClick={() => {
+        // setSelectedCategory(category)
+      }}>
+      <img
+        src={urlPrefix + "/" + trait.imageName + ".png"}
+        alt={trait.imageName}
+      />
+    </div>
+
+  );
 }
 
 export default TraitSelector
--- src/containers/Traits.tsx
+++ src/containers/Traits.tsx
@@ -1,5 +1,8 @@
-import { Fragment, useContext } from "react"
+import { Fragment, useContext, useEffect, useState } from "react"
+import TraitSelector from "../components/TraitSelector"
+import CategoriesData from "../configs/categoriesData"
 import AppContext from "../store/AppContext"
+import { Trait } from "../types"
 import classes from './Traits.module.scss'
 
 interface TraitsProps { }
@@ -7,10 +10,18 @@ interface TraitsProps { }
 const Traits = (props: TraitsProps) => {
     
     const { selectedCategory} = useContext(AppContext)
+    const [traits, setTraits] = useState<Trait[]>([])
+
+    useEffect(() => {
+        const cat = CategoriesData.find((cat) => cat.name === selectedCategory.name)
+        setTraits(cat?.traits || [])
+    }, [selectedCategory])
     return (
         <Fragment>
             <div id="Traits" className={classes.Traits}>
-                {selectedCategory.name}
+                {
+                    traits.map((trait) => <TraitSelector trait={trait}/>)
+                }
             </div>
         </Fragment>
     )
